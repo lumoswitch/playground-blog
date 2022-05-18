653149140ac060611bbc494ff24b1da24649c4e3
lumoswitch <sofrona.anna@gmail.com>
2022-05-18 18:36:40 +0300

    dummy search

--- js/index.js
+++ js/index.js
@@ -1,9 +1,45 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+function searchBlog() {
+    var input, filter, ul, li, a, i, txtValue;
+    document.getElementById('popular-searches').style.display = 'block';
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
+
 }
 
+document.addEventListener('mouseup', function (e) {
+    let container = document.getElementById('popular-searches');
+    if (!container.contains(e.target)) {
+        container.style.display = 'none';
+    }
+});
+
+function showPopularSearches() {
+    document.getElementById('popular-searches').style.display = 'block';
+}
+
+
+$(document).ready(function () {
+
+    $('#search').click(function() {
+        showPopularSearches();
+    });
+
+    $('#search').keyup(function() {
+        searchBlog();
+    });
+  
+    $('.search-btn').click(function () {
+        document.location.href = "./html/card-category.html";
+    });
+});
